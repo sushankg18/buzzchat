9c1967315c4b26f6bb133aaad228a184c5f1ab9f
sushankg18 <sushankg4@gmail.com>
2024-07-08 23:32:06 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,22 +5,25 @@ import userRouter from './routes/user.routes.js'
 import messaageRouter from './routes/message.routes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-const app = express();
+import {app, server} from './socket/socket.js'
+
 dotenv.config({path : "./env"})
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
     origin : 'http://localhost:3000',
-    credentials : true
+    credentials : true,
 }))
 
 app.use('/api/v1/user',userRouter)
 app.use('/api/v1/message',messaageRouter)
 
 mongoDB_Connection()
-.then(app.listen(process.env.PORT || 4000 , ()=>{
-    console.log("app is listening on port ",process.env.PORT || 3000 )  
+// first I wrote app.listen here but for socket connection we have change it to server.listen because we have to run socket and backend app on same server
+.then(server.listen(process.env.PORT || 4000 , ()=>{
+    console.log("app is listening on port ",process.env.PORT || 4000 )  
 }))
 .catch(
     (error)=>{
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import Login from './components/Login';
 import Signup from './components/Signup';
-
-
+import { useSelector } from 'react-redux';
+import io from 'socket.io-client'
 function App() {
+
+  const [socket , setSocket] = useState(null)
+  const { authUser } = useSelector(store => store.user)
+
+  useEffect(() => {
+    if (authUser){
+      const socket = io('http://localhost:8000',{
+        withCredentials: true,
+      });
+      setSocket(socket)
+    };
+  },[authUser])
+
   return (
     <>
       <ChakraProvider>
